938f75427de23aedd5a54faee0d4377444a8e6f6
jakujan <jan.jakubczyk@interia.pl>
2021-01-26 13:17:37 +0100

    Added changes to display images.

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,25 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+    text_1 = models.TextField(blank=True, null=True)
+    cover_1 = models.ImageField(upload_to='images/',blank=True, null=True)
+    text_2 = models.TextField(blank=True, null=True)
+    cover_2 = models.ImageField(upload_to='images/',blank=True, null=True)
+    text_3 = models.TextField(blank=True, null=True)
+    cover_3 = models.ImageField(upload_to='images/',blank=True, null=True)
+    text_4 = models.TextField(blank=True, null=True)
+    cover_4 = models.ImageField(upload_to='images/',blank=True, null=True)
+    text_5 = models.TextField(blank=True, null=True)
+    cover_5 = models.ImageField(upload_to='images/',blank=True, null=True)
+    text_6 = models.TextField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+    
 
     def publish(self):
         self.published_date = timezone.now()
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+from .views import HomePageView
 
 urlpatterns=[
-    path('',views.post_list, name='post_list')
+    path('',views.post_list, name='post_list'),
+    path('post/<int:pk>/',views.post_detail,name='post_detail'),
+    path('', HomePageView.as_view(), name='home'),
 ]
